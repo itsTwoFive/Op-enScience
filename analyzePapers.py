7bf551690e60b4e3441c8d44adfc1fb6ab1a74aa
itsTwoFive <alvarogmendez25@gmail.com>
2024-03-05 19:45:59 +0100

    fix output files folder

--- analyzePapers.py
+++ analyzePapers.py
@@ -1,4 +1,6 @@
+import os
 from grobid_client_python.grobid_client.grobid_client import GrobidClient
-
+if not os.path.isdir('pdfsOut'):
+    os.makedirs("pdfsOut")
 client = GrobidClient(config_path="./config.json")
 client.process("processFulltextDocument", "pdfs",output="pdfsOut", n=20)
