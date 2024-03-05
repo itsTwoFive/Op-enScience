570da643f9d21d717f71d15effadd11fd79a60d1
itsTwoFive <alvarogmendez25@gmail.com>
2024-03-05 20:51:07 +0100

    asd

---
+++ analyzePapersdocker.py
@@ -0,0 +1,11 @@
+import os
+from OpenScienceApp.OpenScience.grobid_client_python.grobid_client.grobid_client import GrobidClient
+
+if not os.path.isdir('pdfsOut'):
+    os.makedirs("pdfsOut")
+else:
+    for f in os.scandir('pdfsOut'):
+        os.remove(f.path)
+        
+client = GrobidClient(config_path="./config.json")
+client.process("processFulltextDocument", "pdfs",output="pdfsOut", n=20)
