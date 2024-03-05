fc8dac301271b57ce714fd1b3793f79c0b028aef
itsTwoFive <alvarogmendez25@gmail.com>
2024-03-05 19:40:23 +0100

    remove gorobid from repository

--- analyzePapers.py
+++ analyzePapers.py
@@ -1,4 +1,4 @@
-from grobid_client.grobid_client import GrobidClient
+from grobid_client_python.grobid_client.grobid_client import GrobidClient
 
 client = GrobidClient(config_path="./config.json")
 client.process("processFulltextDocument", "pdfs",output="pdfsOut", n=20)
